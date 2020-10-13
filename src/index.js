8fecea61ae6b36ac77384a48356cda787c379c93
LuizFelipeM <muniz.mourafelipe@gmail.com>
2020-10-13 13:31:54 -0300

    Base package components

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import DataGridCard from './DataGridCard'
+import DisplayContainer from './DisplayContainer'
+import SideMenu from './SideMenu'
+import StandardCard from './StandardCard'
+
+export {
+  DataGridCard,
+  DisplayContainer,
+  SideMenu,
+  StandardCard
+}
